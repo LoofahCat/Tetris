48db3c1318a0f4dc40311716680ca7fe85d29a46
Abraham Gunther <abraham.gunther@tecuity.com>
2021-04-14 23:35:03 -0600

    Created content and set up basic variables

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
 
 namespace Tetris
 {
@@ -8,17 +14,49 @@ namespace Tetris
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private List<SoundEffect> soundEffects;
+        private SpriteFont font;
+        Song theme;
+        private Texture2D MenuTexture;
+        private Texture2D GameBackground;
+        public static int[] HighScores { get; set; }
+        public Keys[] KeyConfig { get; set; }
+        public static int currentScore { get; set; }
+        Random random;
+        Keys rotRight;
+        Keys rotLeft;
+        Keys softDrop;
+        Keys hardDrop;
+        Keys moveLeft;
+        Keys moveRight;
+        int screenWidth;
+        int screenHeight;
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            
+            
+
+            Content.RootDirectory = "Content";
+
+            //LoadKeys();
+            //LoadScores();
+            //rotLeft = KeyConfig[1];
+            //rotRight = KeyConfig[2];
+            //thrust = KeyConfig[0];
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.IsFullScreen = false;
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.ApplyChanges();
+            screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 
             base.Initialize();
         }
@@ -27,7 +65,8 @@ namespace Tetris
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            MenuTexture = Content.Load<Texture2D>("Main Menu");
+            GameBackground = Content.Load<Texture2D>("Main Screen");
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,11 +81,12 @@ namespace Tetris
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.Black);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(MenuTexture, new Rectangle((int)(screenWidth/4.4f), 0, screenHeight, screenHeight), Color.White);
 
             base.Draw(gameTime);
+            _spriteBatch.End();
         }
     }
 }
---
+++ GameBoard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris
+{
+    class GameBoard
+    {
+        public bool[,] board;
+
+        GameBoard()
+        {
+            board = new bool[10, 21];
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 21; j++)
+                {
+                    board[i,j] = false;
+                }
+            }
+        }
+
+        private bool checkLine(int lineNum)
+        {
+            bool completeLine = true;
+            for (int i = 0; i < lineNum; i++)
+            {
+                if (board[i, lineNum] == false)
+                {
+                    completeLine = false;
+                    break;
+                }
+            }
+            return completeLine;
+        }
+
+        public void detectLine()
+        {
+            for(int i = 0; i < 21; i++)
+            {
+                if (checkLine(i))
+                {
+                    removeLine(i);
+                }
+            }
+
+        }
+
+        private void removeLine(int lineNum)
+        {
+
+        }
+    }
+}
